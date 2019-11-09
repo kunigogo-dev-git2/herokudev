0b91d2cccdfcbe4a48382c8793e95f13dcf46b34
kunigogo-dev-git2 <kunigogo.dev.git2@gmail.com>
2019-11-09 16:38:40 +0000

    index.js package.json追加

---
+++ index.js
@@ -0,0 +1,5 @@
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3000, () => console.log('Server http://localhost:3000'));
