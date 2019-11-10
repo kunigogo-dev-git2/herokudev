adaf855912ebc38ced46c238c970328c65571814
kunigogo-dev-git2 <kunigogo.dev.git2@gmail.com>
2019-11-10 06:40:02 +0000

    port from env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+let port = process.env.PORT || 5000;
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(5000, () => console.log('Server http://localhost:5000'));
+}).listen(port, () => console.log('Server http://localhost:'+port));
